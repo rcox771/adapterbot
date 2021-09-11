277a18334b09a79c8e312d5d3ecfa8212f84b6b8
rcox771 <russ771@gmail.com>
2021-09-11 15:30:36 -0600

    attempting fix for wiki db loading

--- utils/WoW_retriver.py
+++ utils/WoW_retriver.py
@@ -1,16 +1,25 @@
 from drqa import retriever
 from nltk import tokenize
+from pathlib import Path
+
+
+DB_PATH = "data/db"
+Path(DB_PATH).mkdir(parents=True, exist_ok=True)
 
 db = retriever.DocDB()
 print("Loading Wikipedia database")
-db.__init__('retriever/doc_ret/DrQA/data/wikipedia/docs.db')
+db.__init__(f"{DB_PATH}/wiki_docs.db")
 
 
 def fetch_text(doc_id):
     return db.get_doc_text(doc_id)
 
+
 print("Loading Ranker index")
-ranker = retriever.get_class('tfidf')(tfidf_path='retriever/doc_ret/DrQA/data/wikipedia/docs-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz')
+ranker = retriever.get_class("tfidf")(
+    tfidf_path="retriever/doc_ret/DrQA/data/wikipedia/docs-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz"
+)
+
 
 def get_doc(sentence):
     try:
@@ -21,24 +30,25 @@ def get_doc(sentence):
     except:
         return None, [0]
 
+
 def get_KB_WoW(history):
     try:
         para, doc_scores = get_doc(history[-1])
         print("FIRST DOC")
         print(para)
         print(doc_scores[0])
-        if(doc_scores[0]<170):
+        if doc_scores[0] < 170:
             para, doc_scores = get_doc(history[-2])
             print("SECOND DOC")
 
             print(para)
             print(doc_scores[0])
-            if(doc_scores[0]<200):
+            if doc_scores[0] < 200:
                 para, doc_scores = get_doc(history[-3])
                 print("THIRD DOC")
                 print(para)
                 print(doc_scores[0])
-                if(doc_scores[0]<170):
+                if doc_scores[0] < 170:
                     return [""]
                 else:
                     return [para]
